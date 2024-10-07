04b90f2d676784e7b21df7256a42cbc2e9ca8978
Kathryn Verkhohliad <verkhogliadkate@gmail.com>
2024-10-07 23:54:30 +0200

    initial commit

---
+++ TodoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by Kathryn Verkhogliad on 07.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
